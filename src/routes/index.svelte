bd206d70ab24cab9b47fee810536e97e9106e8bc
Cameron Lane <crlane@adamanteus.com>
2021-08-14 14:23:34 +0000

    svelte initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,19 @@
+<svelte:head>
+  <title>crlane.github.io</title>
+</svelte:head>
+
+<script>
+</script>
+
+<h1>crlane.github.io</h1>
+
+<style>
+  :global(body) {
+    font-family: Helvetica, sans-serif;
+  }
+
+  h1 {
+    color: black;
+    font-size: 2em;
+  }
+</style>
